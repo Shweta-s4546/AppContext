fe850e8e52e8845a729e83a3f46688fe6bab5ac5
shweta-s4546 <shweta8873@gmail.com>
2023-06-18 13:42:21 +0530

    First commit of react context

---
+++ src/Context.jsx
@@ -0,0 +1,24 @@
+import React,  { createContext} from "react";
+import useUserApi from "./API/UserApi";
+import usePostApi from "./API/PostApi";
+import useCommentApi from "./API/CommentApi";
+
+//init context
+export const DataContext = createContext();
+
+//context provider components
+function DataProvider(props){
+    const data = {
+        userApi : useUserApi(),
+        postApi : usePostApi(),
+        commentApi : useCommentApi()
+    }
+
+    return(
+        <DataContext.Provider value={data}>
+            { props.children }
+        </DataContext.Provider>
+    )
+}
+
+export default DataProvider
